2dea2da19e70094bf863a1e6bcdbd078731c4977
ygbaldizon7 <54856417+ygbaldizon7@users.noreply.github.com>
2025-08-22 11:47:43 -0600

    Se agrego funcionaliad de Kelvin

--- 3MLIDTS-YairGonzalez-03cs/Form1.Designer.cs
+++ 3MLIDTS-YairGonzalez-03cs/Form1.Designer.cs
@@ -35,9 +35,9 @@ namespace _3MLIDTS_YairGonzalez_03cs
             this.lbCelsius = new System.Windows.Forms.Label();
             this.lbFahrenheint = new System.Windows.Forms.Label();
             this.lbKelvin = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbCelsius = new System.Windows.Forms.TextBox();
+            this.tbFahrenheit = new System.Windows.Forms.TextBox();
+            this.tbKevin = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnCalcular
@@ -106,35 +106,35 @@ namespace _3MLIDTS_YairGonzalez_03cs
             this.lbKelvin.TabIndex = 5;
             this.lbKelvin.Text = "Kelvin :";
             // 
-            // textBox1
+            // tbCelsius
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.tbCelsius.Location = new System.Drawing.Point(146, 69);
+            this.tbCelsius.Name = "tbCelsius";
+            this.tbCelsius.Size = new System.Drawing.Size(100, 20);
+            this.tbCelsius.TabIndex = 6;
             // 
-            // textBox2
+            // tbFahrenheit
             // 
-            this.textBox2.Location = new System.Drawing.Point(146, 104);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
+            this.tbFahrenheit.Location = new System.Drawing.Point(146, 104);
+            this.tbFahrenheit.Name = "tbFahrenheit";
+            this.tbFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.tbFahrenheit.TabIndex = 7;
             // 
-            // textBox3
+            // tbKevin
             // 
-            this.textBox3.Location = new System.Drawing.Point(146, 139);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 8;
+            this.tbKevin.Location = new System.Drawing.Point(146, 139);
+            this.tbKevin.Name = "tbKevin";
+            this.tbKevin.Size = new System.Drawing.Size(100, 20);
+            this.tbKevin.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 261);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbKevin);
+            this.Controls.Add(this.tbFahrenheit);
+            this.Controls.Add(this.tbCelsius);
             this.Controls.Add(this.lbKelvin);
             this.Controls.Add(this.lbFahrenheint);
             this.Controls.Add(this.lbCelsius);
@@ -156,9 +156,9 @@ namespace _3MLIDTS_YairGonzalez_03cs
         private System.Windows.Forms.Label lbCelsius;
         private System.Windows.Forms.Label lbFahrenheint;
         private System.Windows.Forms.Label lbKelvin;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tbCelsius;
+        private System.Windows.Forms.TextBox tbFahrenheit;
+        private System.Windows.Forms.TextBox tbKevin;
     }
 }
 
--- 3MLIDTS-YairGonzalez-03cs/Form1.cs
+++ 3MLIDTS-YairGonzalez-03cs/Form1.cs
@@ -29,12 +29,47 @@ namespace _3MLIDTS_YairGonzalez_03cs
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Presiono el boton de CALCULAR");
+            //MessageBox.Show("Presiono el boton de CALCULAR");
+            if (tbCelsius.Text != ""||tbFahrenheit.Text != "" || tbKevin.Text !="") {
+                //MessageBox.Show("Celsius");
+                if (!string.IsNullOrEmpty(tbCelsius.Text)) {
+                    MessageBox.Show("Celsius");
+                    //Obtenemos el valor del TextBos Celsius y se convierte de string a float
+                    float Ce = float.Parse(tbCelsius.Text);
+                    float Fa = (Ce * 9.0f / 5.0f) + 32;
+                    tbFahrenheit.Text = Fa.ToString();
+                    float Ke = Ce + 273.15f;
+                    tbKevin.Text = Ke.ToString();
+                } else if (!string.IsNullOrEmpty(tbFahrenheit.Text)) {
+                    MessageBox.Show("Fahrenheit");
+                    float Fa = float.Parse(tbFahrenheit.Text);
+                    float Ce = (Fa - 32) *5.0f / 9.0f;
+                    tbCelsius.Text = Ce.ToString();
+                    float Ke = Ce + 273.15f;
+                    tbKevin.Text = Ke.ToString();
+                } else if (!string.IsNullOrEmpty(tbKevin.Text)) {
+                    MessageBox.Show("Kelvin");
+                    float Ke = float.Parse(tbKevin.Text);
+                    float Ce = Ke -273.15f;
+                    tbCelsius.Text = Ce.ToString();
+                    float Fa = (Ce * 9.0f / 5.0f) + 32;
+                    tbFahrenheit.Text = Fa.ToString();
+                }
+            }
+            else {
+                MessageBox.Show("Ingrese valores para el calculo de la temperatura");
+            }
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Presiono el boton de LIMPIAR");
+            //tbCelsius.Text = "0.0";
+            //tbFahrenheit.Text = "0.0";
+            //tbKevin.Text = "0.0";
+            tbCelsius.Clear();
+            tbFahrenheit.Clear();
+            tbKevin.Clear();
         }
     }
 }
